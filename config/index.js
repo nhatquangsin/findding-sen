0608a4397cf7f8391d1524a20211a29156769852
Quang Truong <quangtruong@Quangs-MBP.net.fpt>
2019-01-27 20:06:00 +0700

    change database

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 export default {
   port: 3000,
-  mongoUrl: "mongodb://localhost:27017/user-api",
+  // mongoUrl: "mongodb://localhost:27017/user-api",
+  mongoUrl:
+    "mongodb://nhatquangsin:Nh@tquang98@ds155132.mlab.com:55132/product-database",
   bodyLimit: "100kb"
 };
